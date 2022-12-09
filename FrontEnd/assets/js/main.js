af4ebd986476550468a4844e17f865984d5888cf
01-Remy <98219152+01-Remy@users.noreply.github.com>
2022-12-09 10:41:24 +0100

    Affichage des travaux
    - Récupération des travaux et catégories depuis le back + affichage
    - CSS des boutons
    - Création du filtrage au click des btn

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -3,6 +3,7 @@
  */
 
 const gallery = document.getElementById("gallery");
+const filtre = document.getElementById("filtre");
 
 class Figure {
   constructor(imageUrl, title) {
@@ -16,7 +17,11 @@ class Figure {
     const newTitle = document.createElement("figcaption");
 
     /* fix pour le CORS error : a revoir, la methode est probablement mauvaise
-    sans le replace : La ressource à l’adresse « http://localhost:5678/images/appartement-paris-v1651287270508.png » a été bloquée en raison de son en-tête Cross-Origin-Resource-Policy (ou de son absence). */
+    sans le replace : 
+    
+    La ressource à l’adresse « http://localhost:5678/images/appartement-paris-v1651287270508.png » a été bloquée en raison de son en-tête Cross-Origin-Resource-Policy (ou de son absence). 
+    
+    */
     newImg.src = this.imageUrl.replace("http://localhost:5678", "../Backend");
 
     newImg.alt = this.title;
@@ -28,25 +33,64 @@ class Figure {
   }
 }
 
-// Recupère les travaux et les place dans un nouvel elem <figure>
-fetch("http://localhost:5678/api/works")
-  .then(function (res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function (works) {
-    console.log(works);
-    for (let work of works) {
-      const newWork = new Figure(work.imageUrl, work.title);
-      newWork.build();
-    }
-  })
-  .catch(function (err) {
-    // Une erreur est survenue
-  });
+// supprime tous les enfants de l'element
+function deleteChild(elem) {
+  while (elem.firstChild) {
+    elem.removeChild(elem.firstChild);
+  }
+}
 
-/**
- * TODO : - fetch categ
- *        - créer filtre
- */
+// récupère les catégories et créer les boutons
+function createBtn() {
+  fetch("http://localhost:5678/api/categories")
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (categories) {
+      for (let categ of categories) {
+        const newCateg = document.createElement("li");
+        newCateg.innerText = categ.name;
+        filtre.appendChild(newCateg);
+
+        newCateg.addEventListener("click", () => {
+          deleteChild(gallery);
+          createFigure(categ.id);
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+
+  // bouton "Tous"
+  filtre.querySelector("li").addEventListener("click", () => {
+    deleteChild(gallery);
+    createFigure("all");
+  });
+}
+
+// Recupère les travaux correspondant et les place dans un nouvel elem <figure>
+function createFigure(id) {
+  fetch("http://localhost:5678/api/works")
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (works) {
+      if (id !== "all") {
+        works = works.filter((work) => {
+          return work.categoryId === id;
+        });
+      }
+
+      for (let work of works) {
+        const newWork = new Figure(work.imageUrl, work.title);
+        newWork.build();
+      }
+    })
+    .catch((err) => console.log(err));
+}
+
+createBtn();
+createFigure("all");
